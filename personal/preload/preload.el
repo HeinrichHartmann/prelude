f4136e5e4d439fb015c85fab3d97f5cd2d2b0c03
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2017-01-15 15:34:06 +0100

    Tramp bug workaround

--- personal/preload/preload.el
+++ personal/preload/preload.el
@@ -1,2 +1,7 @@
 (message "running preload")
+
+;; http://emacs.stackexchange.com/questions/18438/emacs-suspend-at-startup-ssh-connection-issue
+(setq tramp-ssh-controlmaster-options
+      "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+
 (setq prelude-theme nil)
