f9487ced03dd71b6a8f4bd2b070de0490a2e964b
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2021-01-16 23:00:29 +0100

    Remove theme setting

--- personal/preload/theme.el
+++ personal/preload/theme.el
@@ -1,13 +1,1 @@
 ;; (setq prelude-theme nil)
-
-(defun containsp (entry list)
-  (numberp (position entry list)))
-
-(defun havethemep (theme)
-  (containsp theme (custom-available-themes)))
-
-(defun set-prelude-theme (theme)
-  (if (havethemep 'theme)
-      (setq prelude-theme 'theme)))
-
-(set-prelude-theme 'spacemacs-light)
