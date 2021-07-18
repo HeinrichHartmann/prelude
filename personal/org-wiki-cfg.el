6267ca3050403361b8919e8ae4512d795e6d2a5a
Heinrich Hartmann <derhein@gmail.com>
2021-07-18 19:08:56 +0000

    fix org-wiki-cfg

--- personal/org-wiki-cfg.el
+++ personal/org-wiki-cfg.el
@@ -1,9 +1,9 @@
 ;; (add-to-list 'load-path "~/.emacs.d/vendor/org-wiki")
 
 (use-package org-wiki
+  :requires (helm ox-html)
   :init
   (setq org-wiki-location-list '("~/org/wiki"))
   ;; Initialize first org-wiki-directory or default org-wiki
   (setq org-wiki-location (car org-wiki-location-list))
   )
-
