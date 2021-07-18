677bcd7d77ffa076676fcdc7998212539eb8849e
Heinrich Hartmann <heinrich.hartmann@zalando.de>
2021-07-18 13:29:05 +0200

    fix org-wiki

--- personal/org-wiki.el
+++
@@ -1,8 +0,0 @@
-(add-to-list 'load-path "~/.emacs.d/vendor/org-wiki")
-
-(require 'org-wiki)
-
-(setq org-wiki-location-list '("~/org/wiki"))
-
-;; Initialize first org-wiki-directory or default org-wiki 
-(setq org-wiki-location (car org-wiki-location-list))
